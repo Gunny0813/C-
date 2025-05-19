ffabb990e540907d87d1d9387b57e2575be4d90e
gunny0813@naver.com <gunny0813@naver.com>
2025-05-19 13:36:23 +0900

    문제3번해결

--- 열혈코딩공부/MultiArray.c
+++ 열혈코딩공부/MultiArray.c
@@ -3,18 +3,27 @@
 
 int main(void)
 {
-	int A[2][4] = { {1,2,3,4},{5,6,7,8} };
-	int B[4][2];
-	for (int i = 0; i < 2; i++) {
+	int arr[5][5];
+	for (int i = 0; i < 4; i++) {
+		int sum = 0;
 		for (int j = 0; j < 4; j++) {
-			B[j][i] = A[i][j];
+			scanf("%d", &arr[i][j]);
+			sum += arr[i][j];
 		}
+		arr[i][4] = sum;
 	}
-	for (int i = 0; i < 2; i++) {
-		for (int j = 0; j < 4; j++) {
-			printf("%d ", B[j][i]);
+	for (int  p= 0;  p< 5; p++) {
+		int sum = 0;
+		for (int q = 0; q < 4; q++) {
+			sum += arr[q][p];
 		}
+		arr[4][p] = sum;
+	}
+	for (int i = 0; i < 5; i++) {
+		for (int j = 0; j < 5; j++) {
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
 	}
 	
-
 }
